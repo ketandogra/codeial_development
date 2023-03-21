bb374580906b0082d762c352aea80211336ccf0e
ketan dogra <ketandogra1367@gmail.com>
2023-03-21 23:53:45 +0530

    File upload - uploads user's profile picture in profile page and display it

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.use(cookieParser());
 
 app.use(express.static("./assets"));
 
+// make the uploads available to the browser
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
 app.set("layout extractStyles", true);
--- models/user.js
+++ models/user.js
@@ -1,24 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
 
-const userSchema = new mongoose.Schema({
+const AVATAR_PATH = path.join("/uploads/users/avatars");
+
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     name: {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// define storage for file
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "..", AVATAR_PATH));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
 });
 
+//static functions for the user
+//single function says that only one instance or file will be uploaded for the fieldname avtar
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single(
+  "avatar"
+);
 
-const User = mongoose.model('User', userSchema);
+// use for AVATAR_PATH will be publicly available for user model
+userSchema.statics.avatarPath = AVATAR_PATH;
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
